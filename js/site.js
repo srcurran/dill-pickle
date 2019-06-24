5beeab242dd89e2fbbdbcc556ed5d02dd2325b57
Sean Curran <srcurran@gmail.com>
2019-06-23 23:26:53 -0400

    first commit

---
+++ js/site.js
@@ -0,0 +1,36 @@
+$(function(){
+	$(".scroll").click(function(event){
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+});
+
+
+$(function() { // document ready
+	if ($('#sub-nav ul').length) { // make sure "#sticky" element exists
+		var el = $('#sub-nav ul');
+		var stickyTop = el.offset().top; // returns number
+		var stickyHeight = el.height();
+
+		$(window).scroll(function() { // scroll event
+			var limit = $('#footer').offset(top) - stickyHeight - 20;
+
+			var windowTop = $(window).scrollTop(); // returns number
+
+			if (stickyTop < windowTop) {
+				el.css({
+					position: 'fixed',
+					top: 0
+				});
+			} else {
+				el.css('position', 'static');
+			}
+
+			if (limit < windowTop) {
+				var diff = limit - windowTop;
+				el.css({
+					top: diff
+				});
+			}
+		});
+	}
+});
